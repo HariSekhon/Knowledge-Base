9143b8a1601b37e50f603d50cb3e1aaea5ccc023
Hari Sekhon <harisekhon@gmail.com>
2024-09-16 19:36:46 +0200

    added uncommitted.sh

---
+++ uncommitted.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-10 14:46:42 +0300 (Sat, 10 Aug 2024)
+#
+#  https///github.com/HariSekhon/Knowledge-Base
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
+
+git status --porcelain | grep ^?? | cut -c 4- | xargs wc -l | sort -k1n
