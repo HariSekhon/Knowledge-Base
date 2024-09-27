4cdf9f910cfa5377bf20a4b6e1185aa34b95e7a2
Hari Sekhon <harisekhon@gmail.com>
2024-09-27 18:40:41 +0100

    updated country_count.sh

--- .github/scripts/country_count.sh
+++ .github/scripts/country_count.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Updates the Countries Count field on the travel.md page
+Updates the Country Count field on the travel.md page
 "
 
 # used by usage() in lib/utils.sh
